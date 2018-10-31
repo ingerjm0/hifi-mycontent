8aec45afd461b72b5343cee5f1a22ecbab1d80dd
Jenna <42979611+ingerjm0@users.noreply.github.com>
2018-10-31 14:58:38 -0700

    Fixed script

--- scripts/interactions-example.js
+++ scripts/interactions-example.js
@@ -1,14 +1,13 @@
 (function(){
-    this.enterEntity = function(entityID) {
-        print(\"enterEntity(\"+entityID.id+\")\");
-        Entities.editEntity(entityID, {
-            color: { red: 255, green: 64, blue: 64 },
-        });
-    };
-    this.leaveEntity = function(entityID) {
-        print(\"leaveEntity(\"+entityID.id+\")\");
-        Entities.editEntity(entityID, { 
-            color: { red: 255, green: 190, blue: 20}
-        });
-    };
+   this.enterEntity = function(entityID) {
+       print("enterEntity("+entityID.id+")");
+       Entities.editEntity(entityID, {
+           color: { red: 255, green: 64, blue: 64 },
+       });
+   };
+   this.leaveEntity = function(entityID) {
+       Entities.editEntity(entityID, {
+           color: { red: 255, green: 190, blue: 20}
+       });
+   };
 });
